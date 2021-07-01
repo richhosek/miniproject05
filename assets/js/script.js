67cb75684bb2e0647c47c567b858de00bb9b4cad
Rich Hosek <richhosek@gmail.com>
2021-06-30 21:21:05 -0500

    Delete Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
 const projectNameInput = $("#project-name");
 const projectTypeSelect = $("#project-type");
 const hourlyRateInput = $("#hourly-rate");
-const dueDate = $("#due-date");
+const dueDateInput = $("#due-date");
 
 $("#submit-button").on("click", function(event) {
     console.log("Submit Project")
     event.preventDefault();
-    const daysLeft = moment(dueDate.val()).dayOfYear() - moment().dayOfYear();
+    const daysLeft = moment(dueDateInput.val()).dayOfYear() - moment().dayOfYear();
     displayProject(
         projectNameInput.val(), 
         projectTypeSelect.val(),
         hourlyRateInput.val(),
-        dueDate.val(),
+        dueDateInput.val(),
         daysLeft,
         daysLeft * 8 * hourlyRateInput.val()
-    )
+    );
+    projectNameInput.val("");
+    projectTypeSelect.val("");
+    hourlyRateInput.val("");
+    dueDateInput.val("")
 });
 
 function displayProject(name, type, rate, date, daysLeft, earnings) {
@@ -26,7 +30,12 @@ function displayProject(name, type, rate, date, daysLeft, earnings) {
             $("<td>").text(rate),
             $("<td>").text(date),
             $("<td>").text(daysLeft),
-            $("<td>").text(earnings)
+            $("<td>").text(earnings),
+            $("<td>").append($("<button>").addClass("btn btn-danger delete").text("Delete"))
         )
     );
 }
+
+$("#project-list").on("click", ".delete", function(event){
+    $(event.target).parent().parent().remove();
+})
