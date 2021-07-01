a1d1b2dc6af377c26462474cc3e068b0088056a8
Rich Hosek <richhosek@gmail.com>
2021-06-30 21:36:29 -0500

    Added Bootstrap Icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,19 @@ const projectTypeSelect = $("#project-type");
 const hourlyRateInput = $("#hourly-rate");
 const dueDateInput = $("#due-date");
 
+// Add a new project to the list when we click on the Submit button
 $("#submit-button").on("click", function(event) {
     console.log("Submit Project")
     event.preventDefault();
     const daysLeft = moment(dueDateInput.val()).dayOfYear() - moment().dayOfYear();
+    const hourlyRate = hourlyRateInput.val();
     displayProject(
         projectNameInput.val(), 
         projectTypeSelect.val(),
-        hourlyRateInput.val(),
+        hourlyRate,
         dueDateInput.val(),
         daysLeft,
-        daysLeft * 8 * hourlyRateInput.val()
+        daysLeft * 8 * hourlyRate
     );
     projectNameInput.val("");
     projectTypeSelect.val("");
@@ -21,6 +23,7 @@ $("#submit-button").on("click", function(event) {
     dueDateInput.val("")
 });
 
+// Creates a new project row in the table
 function displayProject(name, type, rate, date, daysLeft, earnings) {
     $("#project-list")
     .append($("<tr>")
@@ -31,11 +34,12 @@ function displayProject(name, type, rate, date, daysLeft, earnings) {
             $("<td>").text(date),
             $("<td>").text(daysLeft),
             $("<td>").text(earnings),
-            $("<td>").append($("<button>").addClass("btn btn-danger delete").text("Delete"))
+            $("<td>").append($("<button>").addClass("btn btn-danger delete").html("<i class='bi bi-trash'></i> Delete"))
         )
     );
 }
 
+// Deletes a row in the table when you click on its delete button
 $("#project-list").on("click", ".delete", function(event){
     $(event.target).parent().parent().remove();
 })
